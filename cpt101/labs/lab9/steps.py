a448e21db2d4daba7f584b63d6f96d6095582ee3
parkco34 <parkercorya@gmail.com>
2022-11-30 20:02:07 -0500

    Lab0 done... I think

--- cpt101/labs/lab9/steps.py
+++ cpt101/labs/lab9/steps.py
@@ -3,6 +3,31 @@
 # CPT101
 # Average Steps Taken
 """
-
+Reads file, then displays the average steps taken for each month
 """
+
+def avg_steps(month):
+    steps = []
+    file = open("steps.txt", "r")   # Create file object
+    contents = file.read()
+    file.close()    # Close file object
+    step_list = [int(i) for i in contents.split()]    # Creates a list of the steps from the txt
+    # Returns the average steps pr day for the month
+    for day in range(month):
+        steps.append(step_list[day])
+
+    return int(sum(steps) / month)
+
+def main():
+    # 
+    # Days in the sorted months
+    MONTHS = ["JAN", "FEB", "MAR", "APR", "MAY", "JUNE", "JULY", "AUG", "SEPT",
+             "OCT", "NOV", "DEC"]
+    days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    for day in days:    # Iterate thru the days corresponding to the month
+        print(f"{avg_steps(day)}\n")
+
+if __name__ == "__main__":
+    main()
 
